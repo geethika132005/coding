93386f280c3a592c6e5c09e6b2693db2ce95e5ae
Geethika <korangigeethika@gmail.com>
2025-12-09 19:44:13 +0530

    Time: 0 ms (100%), Space: 17.9 MB (8.44%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,11 @@
+class Solution:
+    def plusOne(self, digits):
+        # Start from the last digit
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        
+        # If all digits were 9
+        return [1] + digits

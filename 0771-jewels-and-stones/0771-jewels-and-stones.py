a93ae3d7f739845f5921c803923b370ea0771084
Geethika <korangigeethika@gmail.com>
2026-03-16 21:11:26 +0530

    Time: 0 ms (100%), Space: 19.1 MB (96.18%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        ans = 0
+        for i in stones:
+            for j in jewels:
+                if i == j:
+                    ans = ans + 1
+        return ans
+        

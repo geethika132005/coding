e53e3153fbed3f9ba4ea94c26ca806285c0c2457
Geethika <korangigeethika@gmail.com>
2026-05-24 19:39:25 +0530

    Time: 3 ms (83.5%), Space: 19.7 MB (38.65%) - LeetHub

---
+++ 0406-queue-reconstruction-by-height/0406-queue-reconstruction-by-height.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reconstructQueue(self, people):
+
+        people.sort(
+            key=lambda x: (-x[0], x[1])
+        )
+
+        queue = []
+
+        for person in people:
+
+            queue.insert(
+                person[1],
+                person
+            )
+
+        return queue

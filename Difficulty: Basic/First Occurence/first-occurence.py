d1a0c872525ad05b3b72abb3d94cf196c9a2e017
Geethika <korangigeethika@gmail.com>
2026-06-13 15:20:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/First Occurence/first-occurence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def firstOccurence(self, txt, pat):
+
+        n = len(txt)
+        m = len(pat)
+
+        for i in range(n - m + 1):
+
+            if txt[i:i+m] == pat:
+                return i
+
+        return -1
